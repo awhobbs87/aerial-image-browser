81b02b4de0da6669878bc69e761d010a20126a63
Andrew Hobbs <ahobbs@cloudflare.com>
2026-03-16 11:59:43 +1100

    fix(build): remove persistState/prerenderEnvironment from cloudflare adapter, provide API token during build step

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ export default defineConfig({
 
   adapter: cloudflare({
     imageService: 'compile',
-    prerenderEnvironment: 'node',
-    persistState: true,
   }),
 
   integrations: [react()],
